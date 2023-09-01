c2f00af724771a994a887b4b9c24dc84e23324bc
darraghmckeogh <darragh.mckeogh@paconsulting.com>
2023-09-01 09:56:00 +0100

    start page iterated

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,8 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+/*
+
 // V1
 
 router.post('/find-answer', function(request, response) {
@@ -57,5 +59,39 @@ router.post('/find-postcode-answer', function(request, response) {
         response.redirect("/v2/avon-court")
     } else {
         response.redirect("/v2/not-found-postcode")
+    }
+})
+
+*/
+
+// V3
+
+router.post('/find-street-and-town-answer', function(request, response) {
+
+    var find = request.session.data['find']
+    if (find == "Avon Court"){
+        response.redirect("/v3/select-aa3-1ab")
+    } else {
+        response.redirect("/v3/not-found-street-and-town")
+    }
+})
+
+router.post('/find-building-name-and-city-answer', function(request, response) {
+
+    var find = request.session.data['building-name']
+    if (find == "Avon Court"){
+        response.redirect("/v3/select-avon-court")
+    } else {
+        response.redirect("/v3/not-found-building-name-and-city")
+    }
+})
+
+router.post('/find-postcode-answer', function(request, response) {
+
+    var find = request.session.data['postcode']
+    if (find == "W1A 1AB"){
+        response.redirect("/v3/avon-court")
+    } else {
+        response.redirect("/v3/not-found-postcode")
     }
 })
